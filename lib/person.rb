c0f9e9d64de7a7042f495f025964d7fc0d15f0ee
Brian Feldman <brian.feldman42@gmail.com>
2020-09-03 00:53:35 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -9,10 +9,13 @@ class Person
     @name = new_name
   end
   
-  def breed
-    @breed
+  def initialize(job)
+    @job = job
   end
-  def breed=(new_breed)
-    @breed = new_breed
+  def job
+    @job
+  end
+  def job=(new_job)
+    @job = new_job
   end
 end
